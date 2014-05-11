f1ab7304a96918b996d15321e4f78d67a6efd5c1
Makoto Hashigaya <kfr04546@nifty.com>
2014-05-11 15:34:27 +0900

    Add Dictionary viewer

---
+++ aadictview.js
@@ -0,0 +1,57 @@
+﻿dictdata = "";
+function aadict_init(data_filename, result_id)
+{
+	read_dict_data(data_filename);
+}
+function read_dict_data(filename)
+{
+	var req = new XMLHttpRequest();
+	req.open("GET", filename, true);
+	req.onload = function() {
+		dictdata = req.responseText.split(/\n/);
+	}
+	req.send();
+}
+function search(key, result_id)
+{
+	var result = simple_match(key)
+	document.getElementById(result_id).innerHTML = "<div>" + key + "の検索結果</div>" + basic_format(result);
+}
+
+function simple_match(key)
+{
+	var result = new Array();
+	for(i in dictdata) {
+		s = dictdata[i];
+		if(s.indexOf(key)>=0) {
+			result.push(s);
+		}
+	}
+	return result;
+}
+
+function basic_format(result)
+{
+	var s = "";
+	s += "<pre>"
+	for(var i in result) {
+		var r = result[i];
+		r = textToCDATA(r)
+//		r = r.replace(/^[^【]+/, "<a href=\"#\" onclick=\"search('$&', 'result'); return false;\">$&</a>");
+		r = r.replace(/【/g, "\n    【");
+		r = r.replace(/▽/g, "\n        ▽");
+		r = r + "\n \n";
+		s += r;
+	}
+	s += "</pre>";
+	return s;
+}
+
+function textToCDATA(s)
+{
+	s = s.replace(/&/g, "&amp;");
+	s = s.replace(/</g, "&lt;");
+	s = s.replace(/>/g, "&gt;");
+	s = s.replace(/"/g, "&quot;");
+	return s;
+}
